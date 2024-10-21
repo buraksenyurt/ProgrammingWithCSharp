7045b894d694ee144c4411c7d47b8ab1628934a3
buraksenyurt <burakselimsenyurt@gmail.com>
2024-10-21 21:35:10 +0300

    Updated the reporter class

---
+++ Azon.WeatherLib/City.cs
@@ -0,0 +1,14 @@
+﻿namespace Azon.WeatherLib
+{
+    public class City
+    {
+        public string Name { get; private set; }
+        public Temperature Temperature { get; private set; }
+        public City(string name, Temperature temperature)
+        {
+            // Validasyon işlemleri ele alınabilir
+            Name = name;
+            Temperature = temperature;
+        }
+    }
+}
--- Azon.WeatherLib/Reporter.cs
+++ Azon.WeatherLib/Reporter.cs
@@ -2,12 +2,40 @@
 {
     /*
         Bir sınıf kütüphanesi içerisinde anlamsal bütünlüğü bozmadan istediğimiz kadar tip ve tip üyesi ekleyebiliriz.
+        Aşağıdaki sınıfın şehir bazı sıcaklık değerlerini tutan bir yapıda olduğunu varsayalım.
+        İçinde şehir ve sıcaklık değerlerini taşıyacak bir kaba sahip olabilir.
+        Bu bir array olabilir ya da key:value gibi çalışacak bir Dictionary veri yapısı olabilir veya kolayca genişleyebilen bir koleksiyon.
+        Bir başka deyişle List, Dictionary, Array gibi veri modelleri birden fazla şehir,sıcaklık bilgisi için tasarlanabilir.
      */
     public class Reporter
     {
-        public string GetSummary(string city,Temperature temperature)
+        // private City[] cities = new City[10];
+        // İçerisinde City türünden elemanlar barındıracak bir List koleksiyonu.
+        private List<City> cityList = [];
+        public void AddCity(City city)
         {
-            return string.Empty;
+            cityList.Add(city);
+        }
+        public City GetCity(string cityName)
+        {
+            // GetCity metodu kullanılmak istendiğinde çalışma zamanın NotImplementedException fırlatılır(throw)
+            // Exception nesneler try...catch blokları ile yakalanmazlarsa program crash olur
+            // Biz burada "Henüz bu metot tamamlanmadı" manasında kullanmak istedik.
+            throw new NotImplementedException();
+        }
+        public List<City> GetCityList()
+        {
+            return cityList;
+        }
+        public Temperature GetTemperature(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Tüm kayıtlı şehirlerdeki anlık sıcaklık bilgilerini string formatta döndüren bir metot
+        public string GetSummary()
+        {
+            throw new NotImplementedException();
         }
     }
 }
