d4f3a758058a92acdff39435de15b03df8d2bc26
buraksenyurt <burakselimsenyurt@gmail.com>
2024-12-23 13:24:36 +0300

    Initiated delegates samples.

---
+++ Learning.Delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Learning.Delegates;
+
+/*
+    Delegate tipi ile metotları işaret edebiliriz. Bu metotları parametre olarak başla metotlara
+    taşıyabilmebizi, zincir metot dizilimleri oluşturabilmemizi, belli durumlarda çalışma zamanında
+    event tetiklenmesini sağlar. LINQ sorgularında kullanılan birçok genişletme metodu(extension method)
+    Func, Predicate vb delegate türlerini kullanır.
+
+    Bu projede örnek delegate kullanımlarına yer verilmiştir.
+ */
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
